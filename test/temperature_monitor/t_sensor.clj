e3bbb212e25ce6f0990e08c76909ac036cf92e74
Bryan Ivory <bivory@gmail.com>
2013-05-19 13:10:17 -0700

    QueueSensor getting the id of a sensor

--- test/temperature_monitor/t_sensor.clj
+++ test/temperature_monitor/t_sensor.clj
@@ -5,5 +5,5 @@
 (facts "about QueueSensor"
        (against-background [(around :checks (let [sensor (->QueueSensor 0 [1 2 3 4])] ?form))]
                            (fact "getting the id"
-                                 (get-id sensor) => 0)))
+                                 (get-id (->QueueSensor ..id.. nil)) => ..id..)))
 
