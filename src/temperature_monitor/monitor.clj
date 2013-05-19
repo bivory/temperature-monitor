819170aad0630fb9001905cfd861e981a369b5af
Bryan Ivory <bivory@gmail.com>
2013-05-19 12:30:02 -0700

    create-peak-monitor

--- src/temperature_monitor/monitor.clj
+++ src/temperature_monitor/monitor.clj
@@ -9,6 +9,13 @@
 
 (defrecord-openly ThresholdMonitor [threshhold-fn duration log alarm sensors])
 
+(defn create-peak-monitor
+  "Create a Monitor that sounds an alarm if the temperature readings from two
+   or more sensors are above the threshold temperature for at least two
+   seconds."
+  [threshold duration log alarm sensors]
+  (->ThresholdMonitor (fn [t] (> t threshold)) duration log alarm sensors))
+
 (extend-type ThresholdMonitor Monitor
   (start [this] :undefined)
   (stop [this] :undefined))
