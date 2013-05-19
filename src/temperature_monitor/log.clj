94ebdb1f4c349e4ea3d2f545e26f79646913a516
Bryan Ivory <bivory@gmail.com>
2013-05-19 11:46:09 -0700

    ConsoleLog logging without a sensor

--- src/temperature_monitor/log.clj
+++ src/temperature_monitor/log.clj
@@ -1,7 +1,15 @@
-(ns temperature_monitor.log)
+(ns temperature_monitor.log
+  (:use [midje.open-protocols]))
 
 (defprotocol TemperatureLog
   "Allows temperature readings from the monitored sensors to be recorded."
   (add-entry [this sensor-id temperature timestamp]
              "Creates a new log entry, logging the time at which a temperature
               was read from a sensor."))
+
+(defrecord-openly ConsoleLog [])
+
+(extend-type ConsoleLog TemperatureLog
+  (add-entry [this sensor-id temperature timestamp]
+             {:pre [(number? sensor-id)]}
+             :undefined))
--- test/temperature_monitor/t_log.clj
+++ test/temperature_monitor/t_log.clj
@@ -2,3 +2,7 @@
   (:use midje.sweet)
   (:use [temperature_monitor.log]))
 
+(facts "about logging temperatures"
+       (against-background [(around :checks (let [log (->ConsoleLog)] ?form))]
+                           (fact "logging without a sensor"
+                             (add-entry log nil 0 0) => (throws java.lang.AssertionError))))
