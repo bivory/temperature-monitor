9d97105807795990b078c7ab7d68f6ef1cf6a8b9
Bryan Ivory <bivory@gmail.com>
2013-05-19 13:08:52 -0700

    QueueSensor getting the id of a sensor

--- src/temperature_monitor/sensor.clj
+++ src/temperature_monitor/sensor.clj
@@ -5,3 +5,10 @@
   "A sensor that allows sampling a temperature."
   (get-temperature [this] "Sample of the temperature the sensor is monitoring.")
   (get-id [this] "Get the id of this sensor."))
+
+
+(defrecord-openly QueueSensor [id temperatures])
+
+(extend-type QueueSensor Sensor
+  (get-id [this] (get this :id))
+  (get-temperature [this] :undefined))
--- test/temperature_monitor/t_sensor.clj
+++ test/temperature_monitor/t_sensor.clj
@@ -2,3 +2,8 @@
   (:use midje.sweet)
   (:use [temperature_monitor.sensor]))
 
+(facts "about QueueSensor"
+       (against-background [(around :checks (let [sensor (->QueueSensor 0 [1 2 3 4])] ?form))]
+                           (fact "getting the id"
+                                 (get-id sensor) => 0)))
+
